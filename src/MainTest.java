ccaae225873d7d50aa3bd3af30bfbd4b102b4f20
swsk33 <yanhanhui2001a@163.com>
2020-06-17 20:30:09 +0800

    v1.3

--- src/MainTest.java
+++
@@ -1,8 +0,0 @@
-import swsk33.WFs.*;
-public class MainTest {
-	public static void main(String[] args) throws Exception {
-		String[] ss={"png","jpg","exe"};
-		String s=new FileSaveDialog().createSaveDialog("±£´æÎÄ¼þ",FileSaveDialog.FILE_ONLY,ss);
-		System.out.println(s);
-	}
-}
